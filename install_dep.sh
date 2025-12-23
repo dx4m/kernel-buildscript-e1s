f0058e0e4b3ff36f1fb549af7475db8d2bac2b5e
dx4m <simon@dx3m.org>
2025-12-23 18:11:46 +0100

    update install_dep

--- install_dep.sh
+++ install_dep.sh
@@ -4,7 +4,31 @@
 #
 # Kernel build script for Samsung Galaxy S24 (Exynos)
 
-sudo apt-get update
-sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig python3 -y
-sudo apt-get install repo -y
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+function install_package() {
+    local pkg="$1"
+    if ! dpkg -l | grep -q "^ii  $pkg "; then
+        echo "Installing $pkg..."
+        if ! apt-get install -y "$pkg"; then
+            echo -e "[❌] ${RED}Installation failed: $pkg${NC}" >&2
+            exit 1
+		else
+			echo -e "[✅] ${GREEN}Install success: $pkg${NC}";
+        fi
+    fi
+}
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo -e "[❌] ${RED}This script needs to run as root."
+	echo -e "Try sudo $0 $@ --OR-- sudo !!${NC}"
+	exit 1
+fi
+
+apt-get update && apt-get upgrade -y
+install_package git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig python3 repo
+
 
